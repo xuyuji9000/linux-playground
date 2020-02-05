effe933ddfefef163c4bc77aaeaf0a093b45f478
Karl Xu <karl.yogi.xu@gmail.com>
2020-02-05 11:25:38 +0800

    apply frontend encoding to path input

---
+++ understand-gnu-findutils-locate/playground/frcode/Readme.md
@@ -0,0 +1,7 @@
+- Build `main`: `gcc -o main ./main.c`
+
+- Run `cat input | ./main`
+
+- Prepare input: `find ./ -name "*" >> input`
+
+- Generate output: `cat input | ./main > output`
--- understand-gnu-findutils-locate/playground/frcode/main.c
+++ understand-gnu-findutils-locate/playground/frcode/main.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+int common_prefix(char *oldpath, char *path) {
+    int start;
+    for(start=(int)oldpath; 0 != oldpath[0] && 0 != path[0] && oldpath[0] == path[0]; oldpath++, path++) {}
+    return (int)oldpath - start;
+}
 
 int main() {
-    size_t buffer_length=100;
-    char *buffer = (char *)malloc(buffer_length);
+    size_t path_length=100;
+
+    char *oldpath, *path;
+    path = (char *)malloc(path_length);
+    oldpath = (char *)malloc(path_length);
+    
     int delimiter = '\n';
 
-    while (getdelim (&buffer, &buffer_length, delimiter, stdin) > 0)
+    while (getdelim (&path, &path_length, delimiter, stdin) > 0)
     {
-        printf("%s", buffer);
+        // printf("%s", oldpath);
+        int count = common_prefix(oldpath, path);
+        // printf("Common Prefix length:%d , different suffix: %s\n", count, path+count);
+        printf("%d %s", count, path+count);
+        strcpy(oldpath, path);
+
     }
+
+    free(path);
     return 0;
 }
