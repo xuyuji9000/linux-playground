de97ad34dbab66aa9f5ff456ec94fadac581819f
Karl Xu <karl.yogi.xu@gmail.com>
2020-06-30 10:54:23 +0800

    socket example: first commmit

---
+++ socket/main.c
@@ -0,0 +1,26 @@
+// build command `gcc -o main ./main.c`
+
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
+
+
+
+
+/*
+  Reference: 
+  - [C Socket Programming for Linux with a Server and Client Example Code](https://www.thegeekstuff.com/2011/12/c-socket-programming/)
+*/
