53a1f9900ac23feb3f8914390d3619fe89067a82
Karl Xu <karl.yogi.xu@gmail.com>
2020-09-12 10:15:34 +0800

    execve: print out environment variable

--- systemcalls/execve/main.c
+++ systemcalls/execve/main.c
@@ -1,5 +1,6 @@
 // Reference      : https://man7.org/linux/man-pages/man2/execve.2.html
-// Build command  : gcc ./main.c -o main
+// Build main     : gcc ./main.c -o main
+// Build myecho   : gcc ./myecho.c -o myecho
 // Running command: ./main ./myecho
 
 #include <stdio.h>
@@ -8,7 +9,7 @@
 
 int main( int argc, char *argv[] ) {
     char *newargv[] = { NULL, "hello", "world", NULL };
-    char *newenviron[] = { NULL };
+    char *newenviron[] = { "key=value", NULL };
 
     if ( argc != 2 ) {
         fprintf( stderr, "Usage: %s <file-to-exec>\n", argv[0] );
--- systemcalls/execve/myecho.c
+++ systemcalls/execve/myecho.c
@@ -1,13 +1,21 @@
 // Reference    : https://man7.org/linux/man-pages/man2/execve.2.html
-// Build command: gcc ./myecho.c myecho
+/**
+ * Logic:
+ * - print arguments
+ * - print environment variables
+ **/
+
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[])
+int main (int argc, char *argv[], char *envp[])
 {
-    int j;
-    for(j = 0; j < argc; j++)
+    
+    for(int j = 0; j < argc; j++)
         printf("argv[%d]: %s \n", j, argv[j]);
+    
+    for( int i=0; envp[i] != NULL; i++)
+        printf("%s\n", envp[i]);
     exit(EXIT_SUCCESS);
 }
