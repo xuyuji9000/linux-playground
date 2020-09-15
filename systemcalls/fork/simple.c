b1e6382da28713ced11c8bfc2e4ba9f5db5f3567
Karl Xu <karl.yogi.xu@gmail.com>
2020-09-15 14:29:51 +0800

    fork: optimize simple example

--- systemcalls/fork/simple.c
+++ systemcalls/fork/simple.c
@@ -2,15 +2,25 @@
  * Build Command: gcc ./simple.c -o simple
  **/
 
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 int main() {
-    int child_pid = fork();
+    pid_t child_pid = fork();
+    int child_status;
     if ( 0 == child_pid) {
-        printf("This is child process\n");
+        printf("Unknown command\n");
+        exit(1);
     } else {
-        printf("This is parent process\n");
+
+        pid_t tpid;
+        do {
+            tpid = wait(&child_status);
+        } while( tpid != child_pid);
+        printf("Parent process finish.\n");
+        return child_status;
     }
-    return 0;
 }
+
