6b0b35715113edded0bda588bfd24b65003512f3
Karl Xu <karl.yogi.xu@gmail.com>
2020-09-17 21:20:41 +0800

    bash: receive parameters

---
+++ bash/how-to-receive-arguments/custom.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+POSITIONAL=()
+
+function usage {
+  echo "Help information:
+--arg1 This is first argument
+--arg2 This is second argument
+  "
+}
+
+while [[ $# -gt 0 ]]
+do 
+key="$1"
+
+case $key in
+  --arg1)
+  ARG1="$2"
+  shift
+  shift 
+  ;;
+  --arg2)
+  ARG1="$2"
+  shift
+  shift 
+  ;;
+  -h)
+  usage
+  exit 0
+  ;;
+  *)
+  echo "Unknow parameter."
+  usage
+  exit 1
+  ;;
+esac
+done
