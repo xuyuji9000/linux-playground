7702aaab5b9eaa1b8725218e5f42cd735698eee4
Karl Xu <karl.yogi.xu@gmail.com>
2021-04-05 10:16:55 +0800

    optimize script

--- build-linux-kernel/prepare-dependencies.sh
+++ build-linux-kernel/prepare-dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-dnf group install "Development Tools"
-dnf install ncurses-devel
-dnf install elfutils-libelf-devel
-dnf install openssl-devel
+dnf group install "Development Tools" -y
+dnf install ncurses-devel -y
+dnf install elfutils-libelf-devel -y
+dnf install openssl-devel -y
 
 # Repository PowerTools by default is not enabled
 # use `dnf repolist --all` to check for more info.
 dnf config-manager --set-enabled PowerTools
-dnf install dwarves
+dnf install dwarves -y
