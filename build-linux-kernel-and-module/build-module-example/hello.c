b99c0a65dd7accd845c03f5fa326609d164e60c6
Karl Xu <karl.yogi.xu@gmail.com>
2021-04-07 09:39:10 +0800

    optimize build kernel module example

--- build-linux-kernel-and-module/build-module-example/hello.c
+++ build-linux-kernel-and-module/build-module-example/hello.c
@@ -1,11 +1,12 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/sched.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-    printk(KERN_ALERT "Hello, world\n");
+    printk(KERN_ALERT "Hello, world from process %s (pid %i)\n", current->comm, current->pid);
     return 0;
 }
 
