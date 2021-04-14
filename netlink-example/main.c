f7cf7485ea15c71267e61e760c3ea4708f1e313c
Karl Xu <karl.yogi.xu@gmail.com>
2021-04-14 12:47:07 +0800

    update netlink exaple

--- netlink-example/main.c
+++ netlink-example/main.c
@@ -1,7 +1,102 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+
+
+#define MNL_SOCKET_AUTOPID 0
+struct nlmsghdr *nlh = NULL;
+struct iovec iov;
+int sock_fd;
+
+struct mnl_socket {
+    int                fd;
+    struct sockaddr_nl addr;
+};
+
+static struct mnl_socket* mnl_socket_open(int bus) 
+{
+    struct mnl_socket *nl;
+
+    nl = calloc(1, sizeof(struct mnl_socket));
+    if (nl == NULL)
+        return NULL;
+
+    // AF_* here stands for "Address Family"
+    // While PF_* stands for "Protocol Family", it's not used.
+    nl->fd = socket(AF_NETLINK, SOCK_RAW,bus);
+    if (nl->fd == -1) { 
+        free(nl);
+        return NULL;
+    }
+
+    return nl;
+}
+
+
+static int mnl_socket_bind(struct mnl_socket *nl, unsigned int groups, pid_t pid)
+{
+    int ret;
+    socklen_t addr_len;
+
+    nl->addr.nl_family = AF_NETLINK;
+    nl->addr.nl_groups = groups;
+    nl->addr.nl_pid       = pid;
+
+    ret = bind(nl->fd, (struct sockaddr *)&nl->addr, sizeof(nl->addr));
+    if(ret < 0)
+        return ret;
+
+    addr_len = sizeof(nl->addr);
+    ret = getsockname(nl->fd, (struct sockaddr *)&nl->addr, &addr_len);
+    if(ret < 0)
+        return ret;
+
+    if (addr_len != sizeof(nl->addr)) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    if (nl->addr.nl_family != AF_NETLINK) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    return 0;
+}
+
+static int mnl_socket_close(struct mnl_socket *nl)
+{
+    int ret = close(nl->fd);
+    free(nl);
+    return ret;
+}
 
 int main() 
 {
-    printf("Hello World\n");
-    return 0;
+    struct mnl_socket *nl;
+    int ret = 0;
+    
+    // Prepare the file descriptor 
+    // For communicating through rtnetlink
+    nl= mnl_socket_open(NETLINK_ROUTE);
+    if(!nl) 
+    {
+        ret = -errno;
+        goto cleanup;
+    }
+
+    if (mnl_socket_bind(nl, 0, MNL_SOCKET_AUTOPID) < 0) 
+    {
+        ret = -errno;
+        goto cleanup;
+    }
+    printf("unique id for netlink communication: %d \n", nl->addr.nl_pid);
+
+cleanup:
+    if(nl)
+        mnl_socket_close(nl);
+    return ret;
 }
